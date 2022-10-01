900dde10e1c7e644141d5763fa0387b844def1ed
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2022-10-02 02:49:19 +0900

    三角形としてありえない数値や処理不可の数値をはじく

--- src/tutorial1.js
+++ src/tutorial1.js
@@ -1,12 +1,20 @@
 /**
  * 底辺と高さを引数として受け取り、三角形の面積を戻り値として返す関数を書く
  * 小数点とか整数とかの指定はしない
+ * 三角形としてありえない負値ははじく
+ * 大きすぎて処理が丸められてしまうBigNumberははじく
  * @param  {Number} bottom, height
  * @return {Number} area:三角形の面積
  */
-const trianglArea = (bottom, height) => {
-  const area = (bottom * height) / 2;
-  return area;
+const triangleArea = (bottom, height) => {
+  if (bottom < 0 || height < 0) {
+    return "負値は適用外です";
+  } else if (bottom > 9_007_199_254_740_990 || height > 9_007_199_254_740_990) {
+    return "値が大きすぎます";
+  } else {
+    const area = (bottom * height) / 2;
+    return area;
+  }
 };
 
-module.exports = { trianglArea };
+module.exports = { triangleArea };
