4379ba4457551818d7f7c38963d498288df5e081
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2022-10-08 06:35:45 +0900

    0を入力した場合の処理を、元関数およびテストコードに追加 Fixes #1

--- src/tutorial1.js
+++ src/tutorial1.js
@@ -2,6 +2,7 @@
  * 底辺と高さを引数として受け取り、三角形の面積を戻り値として返す関数を書く
  * 小数点とか整数とかの指定はしない
  * 三角形としてありえない負値ははじく
+ * 同じく三角形としてありえない0値ははじく
  * 大きすぎて処理が丸められてしまうBigNumberははじく
  * @param  {Number} bottom, height
  * @return {Number} area:三角形の面積
@@ -11,7 +12,9 @@ const triangleArea = (bottom, height) => {
     return "負値は適用外です";
   } else if (bottom > 9_007_199_254_740_990 || height > 9_007_199_254_740_990) {
     return "値が大きすぎます";
-  } else {
+  } else if (bottom === 0 ||  height === 0){
+    return "三角形の底辺と高さは0を超える値を入力してください";
+  }else {
     const area = (bottom * height) / 2;
     return area;
   }
--- test/tutorial1.test.js
+++ test/tutorial1.test.js
@@ -10,13 +10,6 @@ describe("三角形の面積が計算されているかを確認する", functio
     expect(triangleArea(3, 4)).to.equal(6, "面積は6");
   });
 
-  it("大きな値MAX_SAFE_INTEGERを入力した場合(処理不可のため適用外）", function () {
-    expect(triangleArea(9_007_199_254_740_991, 9_007_199_254_740_991)).to.equal(
-      "値が大きすぎます",
-      "BigNumberはうまく処理できないため面積は4503599627370495とならない"
-    );
-  });
-
   it("小数点第一位を入力した場合:0.4,0.3", function () {
     expect(triangleArea(0.4, 0.3)).to.equal(0.06, "面積は0.06");
   });
@@ -25,6 +18,20 @@ describe("三角形の面積が計算されているかを確認する", functio
     expect(triangleArea(-1, 1)).to.equal(
       "負値は適用外です",
       "負値が三角にはないため適用外"
+    );
+  });
+
+  it("大きな値MAX_SAFE_INTEGERを入力した場合(処理不可のため適用外）", function () {
+    expect(triangleArea(9_007_199_254_740_991, 9_007_199_254_740_991)).to.equal(
+      "値が大きすぎます",
+      "BigNumberはうまく処理できないため面積は4503599627370495とならない"
+    );
+  });
+
+  it("底辺か高さに0を入力した場合（適用外）:0,0", function () {
+    expect(triangleArea(0, 0)).to.equal(
+      "三角形の底辺と高さは0を超える値を入力してください",
+      "どちらかが0の三角形はないので適用外"
     );
   });
 });
