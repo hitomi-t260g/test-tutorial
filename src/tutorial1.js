5c0d90f525adddbc253d72fcbcd13fae3e9cb73a
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2022-10-09 02:01:24 +0900

    エラーをエラーオブジェクトおよびtrycatchにて処理するようにし、早期リターンに変更 Fixes #1

--- src/tutorial1.js
+++ src/tutorial1.js
@@ -8,15 +8,22 @@
  * @return {Number} area:三角形の面積
  */
 const triangleArea = (bottom, height) => {
-  if (bottom < 0 || height < 0) {
-    return "負値は適用外です";
-  } else if (bottom > 9_007_199_254_740_990 || height > 9_007_199_254_740_990) {
-    return "値が大きすぎます";
-  } else if (bottom === 0 ||  height === 0){
-    return "三角形の底辺と高さは0を超える値を入力してください";
-  }else {
+  try {
+    // 適用外のものはthrow new Errorにて、エラーメッセージを投げる
+    if (bottom < 0 || height < 0) {
+      throw new Error("負値は適用外です");
+    }
+    if (bottom > 9_007_199_254_740_990 || height > 9_007_199_254_740_990) {
+      throw new Error("値が大きすぎます");
+    }
+    if (bottom === 0 || height === 0) {
+      throw new Error("三角形の底辺と高さは0を超える値を入力してください");
+    }
+
     const area = (bottom * height) / 2;
     return area;
+  } catch (error) {
+    return error.message;
   }
 };
 
