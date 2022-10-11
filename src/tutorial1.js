dec7fe810d0a622b95b5912b264d97e2f52fb88a
hitomi-t260g <s_t_u_d_i_a@yahoo.co.jp>
2022-10-12 08:53:07 +0900

    try...catchを削除し、テストを修正（途中）

--- src/tutorial1.js
+++ src/tutorial1.js
@@ -8,23 +8,19 @@
  * @return {Number} area:三角形の面積
  */
 const triangleArea = (bottom, height) => {
-  try {
-    // 適用外のものはthrow new Errorにて、エラーメッセージを投げる
-    if (bottom < 0 || height < 0) {
-      throw new Error("負値は適用外です");
-    }
-    if (bottom > 9_007_199_254_740_990 || height > 9_007_199_254_740_990) {
-      throw new Error("値が大きすぎます");
-    }
-    if (bottom === 0 || height === 0) {
-      throw new Error("三角形の底辺と高さは0を超える値を入力してください");
-    }
-
-    const area = (bottom * height) / 2;
-    return area;
-  } catch (error) {
-    return error.message;
+  // 適用外のものはthrow new Errorにて、エラーメッセージを投げる
+  if (bottom < 0 || height < 0) {
+    throw new Error("負値は適用外です");
   }
+  if (bottom > 9_007_199_254_740_990 || height > 9_007_199_254_740_990) {
+    throw new Error("値が大きすぎます");
+  }
+  if (bottom === 0 || height === 0) {
+    throw new Error("三角形の底辺と高さは0を超える値を入力してください");
+  }
+
+  const area = (bottom * height) / 2;
+  return area;
 };
 
 module.exports = { triangleArea };
